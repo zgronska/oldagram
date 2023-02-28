bf834b8cebc4093ce2d206607c7baadcd5b96e37
zgronska <zgronska@gmail.com>
2023-02-28 22:06:11 +0100

    deleted unnecessary lines

--- index.js
+++ index.js
@@ -140,9 +140,6 @@ function likePost(postId) {
   // Update the like counter
   likeCounterEl.innerText = `${post.likes} likes`;
 }
-  // Update the like counter
-  likeCounterEl.innerText = `${post.likes} likes`;
-}
 
 // Trigger the like function on click
 function handleLike() {
